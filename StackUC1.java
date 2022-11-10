dca7ab5de5974369319b97e9686f8bec0e06084a
Sourav <souravkhare34@gmail.com>
2022-11-10 12:03:18 +0530

    Ability to create a Stack of 56->30->70

---
+++ StackUC1.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.stackproblems;
+
+public class StackUC1 {
+    public static void main(String[] args) {
+        StackClass obj = new StackClass();
+        obj.push(70);
+        obj.push(30);
+        obj.push(56);
+        obj.Print();
+    }
+}
